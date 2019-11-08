a1716e5186aca1945630131727b5fe8562b0422e
ArnodDadayan <dadayanaa@yahoo.com>
2019-11-08 23:24:59 +0400

    refactor

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const data = {
+  id: null,
   name: '',
-  content: '',
-  description: ''
+  description: '',
+  content: ''
 }
 
 const App = () => {
     const [ state, setState ] = useState({ events: [], template: { ...data } });
     const { events } = state;
-    
 
     const handelInputChange = (event) => {
       const { name, value } = event.target;
@@ -32,10 +32,14 @@ const App = () => {
     const handleButtonSubmit = () => {
       const { template, events } = state;
       if(template.name && template.description) {
+        const newTemplate = {
+          ...template,
+          id: new Date().valueOf()
+        }
         setState({
           ...state,
-          events   : [...events, template],
-          template : {}
+          events   : [...events, newTemplate],
+          template : { ...data }
         })
       }
     }
@@ -44,24 +48,24 @@ const App = () => {
       <div className="App">
         <div className="create-event">
           <div className="form-group row">
-            <input type="text" onChange={handelInputChange} value={name} name="name" className="form-control" id="name" placeholder="Event Name" />
+            <input type="text" value={name} onChange={handelInputChange} name="name" className="form-control" id="name" placeholder="Event Name" />
           </div>
           <div className="form-group row">
-            <input type="text" onChange={handelInputChange} value={description} name="description" className="form-control" id="description" placeholder="Description" />
+            <input type="text" value={description} onChange={handelInputChange}  name="description" className="form-control" id="description" placeholder="Description" />
           </div>
           <div className="form-group row">
-            <input type="text" onChange={handelInputChange} value={content} name="content" className="form-control" id="content" placeholder="Content" />
+            <input type="text"value={content} onChange={handelInputChange} name="content" className="form-control" id="content" placeholder="Content" />
           </div>
           <button type="button" onClick={handleButtonSubmit} className="btn btn-primary">Create</button>
         </div>
         <div className="events-tabel">
-         {events.map((item) => {
+         {events.map((event) => {
            return (
-            <div key={item.name} className="card">
+            <div key={event.id} className="card">
               <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{item.description}</h6>
-                <p className="card-text">{item.content}</p>
+                <h5 className="card-title">{event.name}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{event.description}</h6>
+                <p className="card-text">{event.content}</p>
               </div>
             </div>
            )
